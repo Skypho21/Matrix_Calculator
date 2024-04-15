156e35053cda2e50a3215b3593f675408f36fc86
skyfa <skinthav@millersville.edu>
2024-04-15 13:19:18 -0400

    fixed #endif in .hpp

--- MatrixOperations.hpp
+++ MatrixOperations.hpp
@@ -8,12 +8,12 @@ using std::vector;
 
 void displayMatrix(const vector<vector<double>>& matrix);
 vector<vector<double>> createMatrix();
-vector<vector<double>> scalarMultiply(const vector<vector<double>>& matrix, double scalar);
-vector<vector<double>> matrixMultiply(const vector<vector<double>>& a, const vector<vector<double>>& b);
+vector<vector<double>> scalarMultiply(vector<vector<double>>& matrix, double scalar);
+vector<vector<double>> matrixMultiply(vector<vector<double>>& a, vector<vector<double>>& b);
 vector<vector<double>> matrixAdd(const vector<vector<double>>& a, const vector<vector<double>>& b);
 vector<vector<double>> matrixSubtract(const vector<vector<double>>& a, const vector<vector<double>>& b);
 vector<vector<double>> gaussJordanElimination(const vector<vector<double>>& a);
 vector<vector<double>> addIdentityMatrix(vector<vector<double>>& a);
 vector<vector<double>> inverse(vector<vector<double>>& a);
 
-#endif MATRIX_OPERATIONS_HPP
+#endif //MATRIX_OPERATIONS_HPP
